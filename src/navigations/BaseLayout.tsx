f1b1f56749dbf7fef1c3deaddc648cdac7f24960
JIEUN <pandaa0628@gmail.com>
2024-03-14 00:37:57 +0900

    kakao 로그인 예제 구현중

--- src/navigations/BaseLayout.tsx
+++ src/navigations/BaseLayout.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import DetailPage from '../pages/DetailPage';
 import LandingPage from '../pages/LandingPage';
+import LoginHandler from '../pages/LoginHandler';
 import Header from '../layouts/Header';
 
 const BaseLayout = () => {
@@ -12,6 +13,7 @@ const BaseLayout = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/detail" element={<DetailPage />} />
+        <Route path="/login/oauth2/callback/kakao" element={<LoginHandler />} />
       </Routes>
     </BrowserRouter>
   );
